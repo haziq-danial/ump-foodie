0c0407554465c2565d68f3de6531ca7605ccdbb5
Haziq Danial <haziqdanial031@gmail.com>
2022-06-25 04:04:27 +0800

    added cart

---
+++ database/migrations/2022_06_24_171053_create_carts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id('cart_id');
+            $table->unsignedBigInteger('cust_id');
+            $table->integer('cart_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
